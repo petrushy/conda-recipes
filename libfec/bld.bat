6c560e0370041145b9ad17f0b51025400baae9a8
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2020-07-12 17:27:14 +0200

    cleanups

--- libfec/bld.bat
+++ libfec/bld.bat
@@ -1,61 +1,28 @@
 setlocal EnableDelayedExpansion
 @echo on
 
-:: Configure
-if "%ARCH%" == "32" (
-  set SLN_PLAT=Win32
-) else (
-  set SLN_PLAT=x64
-)
+:: define NOMINMAX since gnuradio headers expect min/max to be functions not macros
+set "CFLAGS=%CFLAGS% -DNOMINMAX"
+set "CXXFLAGS=%CXXFLAGS% -DNOMINMAX"
 
-if "%VS_YEAR%" == "2015" (
-  set "SLN_FILE=msvc\libusb_2015.sln"
-)
-if "%VS_YEAR%" == "2017" (
-  set "SLN_FILE=msvc\libusb_2017.sln"
-)
-if "%VS_YEAR%" == "2019" (
-  set "SLN_FILE=msvc\libusb_2019.sln"
-)
+:: Make a build folder and change to it
+mkdir build
+cd build
 
-set "MSBUILD_CMD=%VSINSTALLDIR%MSBuild\%VS_VERSION%\Bin\MSBuild.exe"
-if not exist "%MSBUILD_CMD%" (
-  :: Azure has at least VS 2017
-  :: find VS 2017+ installations using vswhere tool
-  echo Searching for MSBuild using vswhere...
-  set "VSWHERE_CMD=C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
-  if not exist "!VSWHERE_CMD!" (
-    set "VSWHERE_CMD=vswhere"
-  )
-  echo vswhere path: !VSWHERE_CMD!
-  for /f "usebackq tokens=*" %%i in (`"!VSWHERE_CMD!" -latest -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
-    set "MSBUILD_CMD=%%i"
-  )
-  if not exist "!MSBUILD_CMD!" (
-    :: try just finding the VS 2017 installation path
-    for /f "usebackq tokens=*" %%i in (`"!VSWHERE_CMD!" -latest -products * -requires Microsoft.Component.MSBuild -version ^[15.0^,16.0^) -property installationPath`) do (
-      set "vsdir=%%i"
-    )
-    set "MSBUILD_CMD=!vsdir!\MSBuild\15.0\Bin\MSBuild.exe"
-    if not exist "!MSBUILD_CMD!" (
-      :: try a sensible default
-      set "MSBUILD_CMD=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
-      if not exist "!MSBUILD_CMD!" (
-        echo Could not find MSBuild.exe
-        exit 1
-      )
-    )
-  )
-  echo MSBuild path: !MSBUILD_CMD!
-)
-
-:: Build
-"%MSBUILD_CMD%" "%SLN_FILE%" ^
-  /p:Configuration="Release" ^
-  /p:Platform="%SLN_PLAT%" ^
-  /verbosity:normal
+:: configure
+:: enable components explicitly so we get build error when unsatisfied
+cmake -G "Ninja" ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+    -DPYTHON_EXECUTABLE:PATH="%PYTHON%" ^
+    -DBoost_NO_BOOST_CMAKE=ON ^
+    -DGR_PYTHON_DIR:PATH="%SP_DIR%" ^
+    -DENABLE_DOXYGEN=OFF ^
+    ..
 if errorlevel 1 exit 1
 
+<<<<<<< HEAD
 :: Install
 copy %SRC_DIR%\%SLN_PLAT%\Release\dll\libfec-1.0.dll %LIBRARY_BIN%\
 if errorlevel 1 exit 1
@@ -67,4 +34,12 @@ copy %SRC_DIR%\%SLN_PLAT%\Release\lib\libfec-1.0.lib %LIBRARY_LIB%\libfec-1.0_st
 if errorlevel 1 exit 1
 mkdir %LIBRARY_INC%\libfec-1.0
 copy %SRC_DIR%\libusb.h %LIBRARY_INC%\libfec-1.0\
+=======
+:: build
+cmake --build . --config Release -- -j%CPU_COUNT%
+if errorlevel 1 exit 1
+
+:: install
+cmake --build . --config Release --target install
+>>>>>>> 5ab0387100e52870f7389aaf8b7c242f76d4bd6f
 if errorlevel 1 exit 1
